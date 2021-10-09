be9640b0c9756e538620e60c2d19b01dac2fc7ef
Deepa <deepa@macbook-air.lan>
2021-10-09 14:58:49 +0530

    week 5 assignment

---
+++ lesson5/predict.py
@@ -0,0 +1,33 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+model_file = 'model1.bin.1'
+dv_file = 'dv.bin.1'
+
+app = Flask('churn')
+
+with open(model_file,'rb') as f_in:
+    model = pickle.load(f_in)
+
+with open(dv_file,'rb') as f_in:
+    dv = pickle.load(f_in)
+
+customer = {"contract": "two_year", "tenure": 12, "monthlycharges": 19.7}
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0,1]
+
+    result = {
+        'churn_probability' : y_pred
+    }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
